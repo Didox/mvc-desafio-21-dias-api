f3c7e3921a4a36a64addc2a903b4946152330a32
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-08-16 06:50:06 -0300

    exemplo de rota

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -18,6 +18,7 @@ namespace mvc.Controllers
             _logger = logger;
         }
 
+        [Route("/alunos")]
         public IActionResult Index()
         {
             return View(Aluno.Todos());
